2be7c542c6da92144ca4e52e45184de273873902
Ilya Kogan <kogan@ohio.edu>
2016-03-29 13:00:31 -0400

    DO NOT MERGE: Backporting ability to load roles from arrays and lists.

--- pac4j-core/src/main/java/org/pac4j/core/authorization/FromAttributesAuthorizationGenerator.java
+++ pac4j-core/src/main/java/org/pac4j/core/authorization/FromAttributesAuthorizationGenerator.java
@@ -15,6 +15,8 @@
  */
 package org.pac4j.core.authorization;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.StringTokenizer;
 
 import org.pac4j.core.profile.CommonProfile;
@@ -28,45 +30,61 @@ import org.pac4j.core.profile.CommonProfile;
  * @since 1.5.0
  */
 public class FromAttributesAuthorizationGenerator<U extends CommonProfile> implements AuthorizationGenerator<U> {
-    
+
     private final String[] roleAttributes;
-    
+
     private final String[] permissionAttributes;
-    
+
     private String splitChar = ",";
-    
+
     public FromAttributesAuthorizationGenerator(final String[] roleAttributes, final String[] permissionAttributes) {
         this.roleAttributes = roleAttributes;
         this.permissionAttributes = permissionAttributes;
     }
-    
+
     public void generate(final U profile) {
         generateAuth(profile, this.roleAttributes, true);
         generateAuth(profile, this.permissionAttributes, false);
     }
-    
+
     private void generateAuth(final U profile, final String[] attributes, final boolean isRole) {
         if (attributes != null) {
             for (final String attribute : attributes) {
                 final Object value = profile.getAttribute(attribute);
-                if (value != null && value instanceof String) {
-                    final StringTokenizer st = new StringTokenizer((String) value, this.splitChar);
-                    while (st.hasMoreTokens()) {
-                        if (isRole) {
-                            profile.addRole(st.nextToken());
-                        } else {
-                            profile.addPermission(st.nextToken());
+                if (value != null) {
+                    if(value instanceof String) {
+                        final StringTokenizer st = new StringTokenizer((String) value, this.splitChar);
+                        while (st.hasMoreTokens()) {
+                            setAuth(profile, st.nextToken(), isRole);
+                        }
+                    } else if(value.getClass().isArray() && value.getClass().getComponentType().isAssignableFrom(String.class)) {
+                        for(Object item : (Object[])value) {
+                            setAuth(profile, item.toString(), isRole);
+                        }
+                    } else if(Collection.class.isAssignableFrom(value.getClass())) {
+                        for(Object item : (Collection<?>)value) {
+                            if(item.getClass().isAssignableFrom(String.class)) {
+                                setAuth(profile, item.toString(), isRole);
+                            }
                         }
                     }
                 }
             }
         }
     }
-    
+
+    private void setAuth(final U profile, final String value, final boolean isRole) {
+        if (isRole) {
+            profile.addRole(value);
+        } else {
+            profile.addPermission(value);
+        }
+    }
+
     public String getSplitChar() {
         return this.splitChar;
     }
-    
+
     public void setSplitChar(final String splitChar) {
         this.splitChar = splitChar;
     }
